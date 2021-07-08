f120d15fb896b055237590e784d0239b3463c5b5
Nazar Yavornytskyy <nazaryavornytskyy@gmail.com>
2021-07-08 10:39:04 +0300

    first commit

---
+++ CarthageAPI/CarthageAPI/CarthageAPI.h
@@ -0,0 +1,18 @@
+//
+//  CarthageAPI.h
+//  CarthageAPI
+//
+//  Created by Nazar Yavornytskyy on 7/8/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CarthageAPI.
+FOUNDATION_EXPORT double CarthageAPIVersionNumber;
+
+//! Project version string for CarthageAPI.
+FOUNDATION_EXPORT const unsigned char CarthageAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarthageAPI/PublicHeader.h>
+
+
---
+++ CarthageAPI/CarthageAPI/CarthageAPI.swift
@@ -0,0 +1,8 @@
+//
+//  CarthageAPI.swift
+//  CarthageAPI
+//
+//  Created by Nazar Yavornytskyy on 7/8/21.
+//
+
+import Foundation
